da04aa50728649e8d64a51ddea1c6a9ba7e97155
SamuelWillis <hello@samuelwillis.dev>
2026-02-14 20:44:29 -0800

    Parse URI exactly once
    This adds some vanilla recursion in order to parse each uri exactly
    once.
    
    There's probably all kinds of improvements here and this will almost
    definitely be way too slow for a big website... but it works.
    
    Pretty much there's 2 buckets:
    
    1. Things to visit
    2. Things that have been visited
    
    The crawling is done when there's nothing left to visit.
    
    Before requesting each URI, a check is done to make sure that the uri
    has not been visited.
    If it has been, we move onto the next URI to visit.
    
    As mentioned above, this works but will be slow. It would be cool if
    some concurrency could be added.

--- lib/web_crawler.ex
+++ lib/web_crawler.ex
@@ -6,29 +6,37 @@ defmodule WebCrawler do
   def crawl(url) do
     uri = URI.new!(url)
 
-    uri
+    [uri]
     |> crawl([])
     |> Enum.map(&URI.to_string/1)
     |> Enum.frequencies()
   end
 
-  defp crawl(uri, visited) do
-    %Req.Response{body: body} = Req.new(url: uri) |> Req.get!()
-    visited = [uri | visited]
+  # If no more to visit, return visited!
+  defp crawl([], visited) do
+    visited
+  end
 
-    found =
-      body
-      |> Floki.parse_document!()
-      |> Floki.find("a[href]")
-      |> Enum.map(&Floki.attribute(&1, "href"))
-      |> Enum.map(&hd/1)
-      |> Enum.map(&URI.merge(uri, &1))
-      |> Enum.reject(&(&1.host != uri.host))
-      |> Enum.reject(&Enum.member?(visited, &1))
-      # Change the recursion somehow so that we add the results of the crawling to the visited for the next call
-      |> Enum.map(&crawl(&1, visited))
-      |> List.flatten()
+  defp crawl([visit | rest], visited) do
+    # If URI to visit is a member of visited, bypass it
+    if visit in visited do
+      crawl(rest, visited)
+    else
+      # Otherwise, visit the URL
+      %Req.Response{body: body} = Req.get!(Req.new(url: visit))
+      visited = [visit | visited]
 
-    [uri | found]
+      new_anchor_uris =
+        body
+        |> Floki.parse_document!()
+        |> Floki.find("a[href]")
+        |> Enum.map(&Floki.attribute(&1, "href"))
+        |> Enum.map(&hd/1)
+        |> Enum.map(&URI.merge(visit, &1))
+        |> Enum.reject(&(&1.host != visit.host))
+
+      # Add whatever uris were found to URIs to visit and keep going!
+      crawl(rest ++ new_anchor_uris, visited)
+    end
   end
 end
