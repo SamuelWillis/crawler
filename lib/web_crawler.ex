69992df2d0226fd5f58aa23d6f4f14232c97ade3
SamuelWillis <hello@samuelwillis.dev>
2026-02-14 21:29:17 -0800

    Do requests concurrently
    Do requests concurrently. Also add a hokey timing function so that I get
    an idea of how much SPEED this is adding to my crawling.
    
    ~Early results show sync runtimes of ~7.5seconds and concurrent runtimes
    of ~1.5seconds.~
    
    ~That was because of a cold start on my website LOL.~
    
    It was that but also because I was forgetting to do concurrent requests
    after the first request in the concurrent case...
    
    Pür speed

--- lib/web_crawler.ex
+++ lib/web_crawler.ex
@@ -6,18 +6,38 @@ defmodule WebCrawler do
   def crawl(url) do
     uri = URI.new!(url)
 
-    [uri]
-    |> crawl([])
-    |> Enum.map(&URI.to_string/1)
-    |> Enum.frequencies()
+    {sync_time, sync_result} =
+      :timer.tc(fn ->
+        [uri]
+        |> crawl([])
+        |> Enum.map(&URI.to_string/1)
+        |> Enum.frequencies()
+      end)
+
+    {concurrent_time, concurrent_results} =
+      :timer.tc(fn ->
+        [uri]
+        |> crawl([], concurrent: true)
+        |> Enum.map(&URI.to_string/1)
+        |> Enum.frequencies()
+      end)
+
+    %{
+      sync_time: sync_time / 1_000_000,
+      sync_result: sync_result,
+      concurrent_time: concurrent_time / 1_000_000,
+      concurrent_results: concurrent_results
+    }
   end
 
   # If no more to visit, return visited!
-  defp crawl([], visited) do
+  defp crawl(visit, visited, opts \\ [concurrent: false])
+
+  defp crawl([], visited, _opts) do
     visited
   end
 
-  defp crawl([visit | rest], visited) do
+  defp crawl([visit | rest], visited, concurrent: false) do
     # If URI to visit is a member of visited, bypass it
     if visit in visited do
       crawl(rest, visited)
@@ -38,5 +58,28 @@ defmodule WebCrawler do
       # Add whatever uris were found to URIs to visit and keep going!
       crawl(rest ++ new_anchor_uris, visited)
     end
+  end
+
+  defp crawl(visit, visited, opts) do
+    uris_to_visit = Enum.reject(visit, &(&1 in visited))
+
+    requests =
+      for uri <- uris_to_visit do
+        Task.async(fn ->
+          %Req.Response{body: body} = Req.get!(Req.new(url: uri))
+
+          body
+          |> Floki.parse_document!()
+          |> Floki.find("a[href]")
+          |> Enum.map(&Floki.attribute(&1, "href"))
+          |> Enum.map(&hd/1)
+          |> Enum.map(&URI.merge(uri, &1))
+          |> Enum.reject(&(&1.host != uri.host))
+        end)
+      end
+
+    new_anchor_uris = requests |> Task.await_many() |> List.flatten()
+
+    crawl(new_anchor_uris, uris_to_visit ++ visited, opts)
   end
 end
