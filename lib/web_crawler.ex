e06da0a1ea34bf1c460972725b08af058b78263c
SamuelWillis <hello@samuelwillis.dev>
2026-03-30 22:21:45 -0700

    Add a depth parameter
    Add a depth parameter to support searching to a specific depth.
    
    In doing this, it was noticed that we actually want to return all the
    links at the given depth without visiting the URIs.
    
    To facilitate this the filtering of repeated URIs was moved so that it
    occurs before the next recursion call, rather at the start of the
    recursive call.
    
    This means that each recursive head recieves a deduplicated list of URIs
    to visit at that depth.

--- lib/web_crawler.ex
+++ lib/web_crawler.ex
@@ -5,68 +5,38 @@ defmodule WebCrawler do
   https://crawler-test.com/
   """
 
-  def crawl(url) do
+  def crawl(url, opts \\ []) do
     uri = URI.new!(url)
 
-    {sync_time, sync_result} =
-      :timer.tc(fn ->
-        [uri]
-        |> crawl([])
-        |> Enum.map(&URI.to_string/1)
-        |> Enum.frequencies()
-      end)
+    max_depth = Keyword.get(opts, :max_depth, :infinity)
 
-    {concurrent_time, concurrent_results} =
-      :timer.tc(fn ->
-        [uri]
-        |> crawl([], concurrent: true)
-        |> Enum.map(&URI.to_string/1)
-        |> Enum.frequencies()
-      end)
-
-    %{
-      sync_time: sync_time / 1_000_000,
-      sync_result: sync_result,
-      concurrent_time: concurrent_time / 1_000_000,
-      concurrent_results: concurrent_results
+    context = %{
+      current_depth: 0,
+      max_depth: max_depth
     }
+
+    [uri]
+    |> crawl([], context)
+    |> Enum.map(&URI.to_string/1)
+    |> Enum.frequencies()
   end
 
   # If no more to visit, return visited!
-  defp crawl(visit, visited, opts \\ [concurrent: false])
+  defp crawl(visit, visited, context)
 
   defp crawl([], visited, _opts) do
     visited
   end
 
-  defp crawl([visit | rest], visited, concurrent: false) do
-    # If URI to visit is a member of visited, bypass it
-    if visit in visited do
-      crawl(rest, visited)
-    else
-      # Otherwise, visit the URL
-      %Req.Response{body: body} = Req.get!(Req.new(url: visit))
-      visited = [visit | visited]
-
-      new_anchor_uris =
-        body
-        |> Floki.parse_document!()
-        |> Floki.find("a[href]")
-        |> Enum.map(&Floki.attribute(&1, "href"))
-        |> Enum.map(&hd/1)
-        |> Enum.map(&URI.merge(visit, &1))
-        |> Enum.reject(&(&1.host != visit.host))
-
-      # Add whatever uris were found to URIs to visit and keep going!
-      crawl(rest ++ new_anchor_uris, visited)
-    end
+  defp crawl(visit, visited, %{current_depth: max_depth, max_depth: max_depth}) do
+    visit ++ visited
   end
 
-  defp crawl(visit, visited, opts) do
-    uris_to_visit = Enum.reject(visit, &(&1 in visited))
+  defp crawl(visit, visited, context) do
+    %{current_depth: current_depth} = context
 
     requests =
-      for uri <- uris_to_visit do
+      for uri <- visit do
         Task.async(fn ->
           %Req.Response{body: body} = Req.get!(Req.new(url: uri))
 
@@ -75,13 +45,21 @@ defmodule WebCrawler do
           |> Floki.find("a[href]")
           |> Enum.map(&Floki.attribute(&1, "href"))
           |> Enum.map(&hd/1)
+          |> Enum.map(&String.replace_suffix(&1, "/", ""))
           |> Enum.map(&URI.merge(uri, &1))
           |> Enum.reject(&(&1.host != uri.host))
         end)
       end
 
-    new_anchor_uris = requests |> Task.await_many() |> List.flatten()
+    visited = visit ++ visited
 
-    crawl(new_anchor_uris, uris_to_visit ++ visited, opts)
+    uris_to_visit =
+      requests
+      |> Task.await_many()
+      |> List.flatten()
+      |> Enum.reject(&(&1 in visited))
+
+    new_context = %{context | current_depth: current_depth + 1}
+    crawl(uris_to_visit, visited, new_context)
   end
 end
